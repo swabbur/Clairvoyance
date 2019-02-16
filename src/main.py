2182af277175d364bca1b74fca1dc85a4451c25c
Wessel van de Brug <wvdbrug@gmail.com>
2019-02-16 20:39:42 +0100

    Extract main operators

--- src/main.py
+++ src/main.py
@@ -1,64 +1,25 @@
-import scipy as sp
-import networkx as nx
-import statistics as st
+import operations as op
 import parsers as pr
+import statistics as st
 
 
 (identifiers, ratings) = pr.from_csv("./res/flavor_ratings.csv")
 (user_count, item_count) = ratings.shape
 
-print("Parsed ratings")
+standardized_ratings = op.standardize_ratings(ratings)
+pairs = op.generate_pairs(user_count)
+measures = [st.pearson_r, st.kendall_tau_b]
 
-standardized_ratings = sp.empty(ratings.shape)
+similarities = op.compute_similarities(standardized_ratings, pairs, measures)
 
-for index, row in enumerate(ratings):
-    standardized_ratings[index] = st.standardize(row)
+threshold = 0.608
+matching = op.match_pairs(pairs, similarities, threshold)
 
-print("Standardized ratings")
-
-pearson_similarities = []
-kendall_similarities = []
-
-for i in range(user_count):
-    for j in range(i + 1, user_count):
-        r = st.pearson_r(standardized_ratings[i], standardized_ratings[j])
-        tau = st.kendall_tau_b(standardized_ratings[i], standardized_ratings[j])
-        pearson_similarities.append(r)
-        kendall_similarities.append(tau)
-
-print("Computed similarities")
-
-pearson_similarities = st.normalize(pearson_similarities)
-kendall_similarities = st.normalize(kendall_similarities)
-
-print("Normalized similarities")
-
-graph = nx.Graph()
-
-index = 0
-for i in range(user_count):
-    for j in range(i + 1, user_count):
-        r = pearson_similarities[index]
-        tau = kendall_similarities[index]
-        similarity = (r + tau) / 2
-        if similarity > 0.608:
-            graph.add_edge(i, j, weight=similarity)
-        index += 1
-
-print("Created network")
-
-matching = nx.algorithms.matching.max_weight_matching(graph, True)
-
-print("Matched pairs")
-
-print()
 for match in matching:
-    identifier_1 = identifiers[match[0]]
-    identifier_2 = identifiers[match[1]]
-    similarity = graph[match[0]][match[1]]["weight"]
-    print("%s %s %.3f" % (identifier_1, identifier_2, similarity))
-
-print(len(matching))
+    identifier_1 = identifiers[match[0][0]]
+    identifier_2 = identifiers[match[0][1]]
+    match_similarity = match[1]
+    print("%s %s %.2f" % (identifier_1, identifier_2, match_similarity))
 
 # TODO: Format output
 # 1.  oonger  1.00    (Boon, Jager)
---
+++ src/operations.py
@@ -0,0 +1,51 @@
+import scipy as sp
+import statistics as st
+import networkx as nx
+
+
+def standardize_ratings(ratings):
+    standardized_ratings = sp.empty(ratings.shape)
+    for index, row in enumerate(ratings):
+        standardized_ratings[index] = st.standardize(row)
+    return standardized_ratings
+
+
+def generate_pairs(n):
+    return [(i, j) for i in range(n) for j in range(i + 1, n)]
+
+
+def compute_similarities(ratings, pairs, measures):
+
+    similarities = []
+
+    for measure in measures:
+        sub_similarities = []
+        for (i, j) in pairs:
+            similarity = measure(ratings[i], ratings[j])
+            sub_similarities.append(similarity)
+        similarities.append(sub_similarities)
+
+    for index in range(len(similarities)):
+        similarities[index] = st.normalize(similarities[index])
+
+    averaged_similarities = []
+    for index in range(len(pairs)):
+        total = 0
+        for similarity in similarities:
+            total += similarity[index]
+        averaged_similarities.append(total / len(similarities))
+
+    return averaged_similarities
+
+
+def match_pairs(pairs, similarities, threshold=0.5):
+
+    graph = nx.Graph()
+
+    for (i, j), similarity in zip(pairs, similarities):
+        if similarity > threshold:
+            graph.add_edge(i, j, weight=similarity)
+
+    matching = nx.algorithms.matching.max_weight_matching(graph, True)
+
+    return [((match[0], match[1]), graph[match[0]][match[1]]["weight"]) for match in matching]
